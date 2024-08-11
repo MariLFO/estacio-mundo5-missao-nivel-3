8a63ff7711858e387869ce1a157e8e5a3879ca1a
MariLFO <mari.fer.onorio@gmail.com>
2024-08-10 20:59:31 -0700

    nome do arquivo alterado

--- microatividade1.py
+++ microatividade1.py
@@ -4,7 +4,7 @@ import pandas as pd
 dados = pd.read_csv('dados.csv', sep=';', engine='python', encoding='utf-8')
 
 # Salva as alterações no arquivo CSV
-dados.to_csv('dados_modificados.csv', index=False)
+dados.to_csv('dados_gerados_microatividade1.csv', index=False)
 
 # Exibe os dados da variável
 print(dados)
