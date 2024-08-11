24a505ae4616381ef3d6078059c9cdedd018c513
MariLFO <mari.fer.onorio@gmail.com>
2024-08-10 20:55:57 -0700

    Microatividade 1

---
+++ microatividade1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Variável para armazenar os dados
+dados = pd.read_csv('dados.csv', sep=';', engine='python', encoding='utf-8')
+
+# Salva as alterações no arquivo CSV
+dados.to_csv('dados_modificados.csv', index=False)
+
+# Exibe os dados da variável
+print(dados)
